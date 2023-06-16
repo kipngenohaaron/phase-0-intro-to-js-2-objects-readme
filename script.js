d1ef91602a75dc8413990fb2a36f4e20ab76f222
kipngenohaaron <kipngenohaaron@gmail.com>
2023-06-16 14:22:38 +0300

    assignment completed

---
+++ script.js
@@ -0,0 +1,15 @@
+const person = {
+    name: "John Doe",
+    age: 25,
+    city: "New York",
+  };
+  // Dot notation
+console.log(person.name); 
+console.log(person.age); 
+console.log(person.city);
+
+// Bracket notation:
+console.log(person["name"]); 
+console.log(person["age"]); 
+console.log(person["city"]); 
+
